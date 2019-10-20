115ab235f6b467f3ec4c16c6155313d8ef4fb117
kamkali <kamkalis@gmail.com>
2019-10-20 09:07:16 +0200

    Better version of Ex1

--- week01_day02/day02.py
+++ week01_day02/day02.py
@@ -5,12 +5,24 @@ doors_count = [2, 3, 4]
 
 def get_engine_list(minimum=1.0, maximum=2.2, step=0.1):
     engine_list = [minimum]
-    while minimum < maximum:
+    while minimum <= maximum:
         minimum += step
-        minimum = round(minimum, 1)
-        engine_list.append(minimum)
+        if minimum <= maximum:
+            minimum = round(minimum, 1)
+            engine_list.append(minimum)
     return engine_list
 
 
-engines = get_engine_list()
+def get_engine_list2(minimum=1.0, maximum=2.2, step=0.1):
+    engine_list = []
+    while minimum <= maximum:
+        minimum = round(minimum, 1)
+        engine_list.append(minimum)
+        minimum += step
+    return engine_list
+
+
+engines = get_engine_list(1.0, 2.3, 0.2)
 print(engines)
+print()
+print(get_engine_list2(1.0, 2.3, 0.2))
