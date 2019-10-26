73b3ec79c3f7a4722614d953e8ae86a240d834dd
kamkali <kamkalis@gmail.com>
2019-10-26 15:00:01 +0200

    W2D1: CSV file exersice done

--- week2_day1/csv_file_exercise.py
+++ week2_day1/csv_file_exercise.py
@@ -1,4 +1,12 @@
 from typing import Sequence, List, Dict
+import day1
+
+
+def print_medals(filename: str = 'ski_jumping_2014_medals.csv'):
+    with open(filename, 'r') as f:
+        for line in f:
+            name, *_, medal = line.strip().split(',')
+            print(f"{name} got {medal.lower()} medal")
 
 
 def read_csv(filename: str = 'ski_jumping_2014_medals.csv') -> List[Dict]:
@@ -12,16 +20,19 @@ def read_csv(filename: str = 'ski_jumping_2014_medals.csv') -> List[Dict]:
     return csv_list
 
 
-def print_medals(filename: str = 'ski_jumping_2014_medals.csv'):
-    with open(filename, 'r') as f:
-        for line in f:
-            name, *_, medal = line.strip().split(',')
-            print(f"{name} got {medal.lower()} medal")
+def type_data(list_data: List[Dict]):
+    for d in list_data:
+        d['Age'], d['Height'], d['Weight'] = int(float(d['Age'])), float(d['Height']), float(d['Weight'])
 
 
 if __name__ == '__main__':
     # print_medals()
     data = read_csv()
+    type_data(data)
     height_list = [d['Height'] for d in data]
     print(height_list)
+
+    h_mean = day1.mean_sdv(height_list)
+    h_mean = round(h_mean[0], 2), round(h_mean[1], 2)
+    print(h_mean)
     # read_csv('ski_jumping_2014_medals_extended.csv')
