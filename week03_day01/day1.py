0fc06c58360a6b8c2988ab4be221d0ceabe0f725
kamkali <kamkalis@gmail.com>
2019-11-16 08:49:52 +0100

    w3d1: update

--- week03_day01/day1.py
+++ week03_day01/day1.py
@@ -1,1 +1,13 @@
-print("Hello")
+from typing import Sequence, List, Dict
+
+
+def multiply_by(my_list, multiplier):
+    return [element * multiplier for element in my_list]
+
+
+def main():
+    print(multiply_by([2, 4, 6], 7))
+
+
+if __name__ == '__main__':
+    main()
