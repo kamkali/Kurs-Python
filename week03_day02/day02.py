cbaa671b1af6a5d848fe5d2023c9961c627518f1
kamkali <kamkalis@gmail.com>
2019-11-17 08:51:09 +0100

    W3D2: Vector

--- week03_day02/day02.py
+++ week03_day02/day02.py
@@ -1,1 +1,21 @@
-print("Init W3D2")
+# Class reminder
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'({self.x},{self.y})'
+
+    def abs(self):
+        return (self.x ** 2 + self.y ** 2) ** (1 / 2)
+
+
+def vector_test():
+    v1 = Vector2D(3,4)
+    print(v1)
+    print(v1.abs())
+
+
+if __name__ == '__main__':
+    vector_test()
