4033859803b3116da72786f8d57755c50fdff9e4
kamkali <kamkalis@gmail.com>
2019-11-17 14:26:03 +0100

    W3D2: Wrappers exercise

--- week03_day02/wrapers.py
+++ week03_day02/wrapers.py
@@ -4,10 +4,6 @@ import time
 ENABLE_WRAPPER = True
 
 
-def main():
-    test_wrapper()
-
-
 def simple_wrapper(func):
     @functools.wraps(func)
     def wrap(*args, **kwargs):
@@ -27,11 +23,13 @@ def my_fun():
     print("Haha")
 
 
-def time_wraper(func):
+def time_wrapper(func):
     @functools.wraps(func)
     def wraps(*args, **kwargs):
         start_time = time.time()
+
         fun_result = func(*args, **kwargs)
+
         end_time = time.time() - start_time
         print('Time elapsed: ', end_time, 's')
         return fun_result
@@ -42,21 +40,41 @@ def time_wraper(func):
         return func
 
 
-@time_wraper
+@time_wrapper
 def long_func():
     for i in range(10 ** 8):
         i *= 2
 
 
+def wrapper_with_message(message):
+    def proper_wrapper(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            print(message)
+            result = func(*args, **kwargs)
+            return result
+
+        return wrapper
+
+    return proper_wrapper
+
+
+@wrapper_with_message(message="Hello")
+def multiply(x, y):
+    return x * y
+
+
 def test_wrapper():
-    my_fun()
-    stime = time.time()
-    long_func()
-    dtime = time.time() - stime
-    print('Czas', dtime, 's')
+    # my_fun()
+    # stime = time.time()
+    # long_func()
+    # dtime = time.time() - stime
+    # print('Czas', dtime, 's')
+
+    print(multiply(2, 3))
 
 
 ########################################################
 
 if __name__ == '__main__':
-    main()
+    test_wrapper()
