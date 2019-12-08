8cc8373f4939a182fa1c18c605084b2c7a42f2b6
kamkali <kamkalis@gmail.com>
2019-12-08 09:32:07 +0100

    W5D2: tkinter grid

--- week05_day02/gui_tkinter.py
+++ week05_day02/gui_tkinter.py
@@ -14,13 +14,37 @@ def simple01():
 
     # Buttons
     def button_action():
-        print(f'Clicked')
+        if label01.cget('text') == 'Hello world':
+            label01.configure(text='Clicked')
+        else:
+            label01.configure(text='Hello world')
 
-    w = tk.Button(main_window, text='button', command=button_action)
-    w.pack(side=tk.LEFT)
+    button01 = tk.Button(main_window, text='button', command=button_action)
+    button01.pack(side=tk.LEFT)
+
+    def button2_action():
+        tk.Label(main_window, text='Add').pack(side=tk.BOTTOM)
+
+    button02 = tk.Button(main_window, text='Second Button', command=button2_action).pack(side=tk.RIGHT)
 
     main_window.mainloop()
 
 
+def simple02():
+    main_wind = tk.Tk()
+    main_wind.title("Second application")
+
+    c1 = tk.Canvas(bg='#ffff00')
+    c1.grid(row=0, column=0)
+    c2 = tk.Canvas(bg='#ff9911')
+    c2.grid(row=0, column=1)
+    c3 = tk.Canvas(bg='#00ffff')
+    c3.grid(row=1, column=0)
+    c4 = tk.Canvas(bg='#ff00ff')
+    c4.grid(row=1, column=1)
+
+    main_wind.mainloop()
+
+
 if __name__ == '__main__':
-    simple01()
+    simple02()
