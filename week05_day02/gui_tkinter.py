725666d607872a85d6102e1d253e234aafac4c17
kamkali <kamkalis@gmail.com>
2019-12-08 10:29:30 +0100

    W5D2: buttons

--- week05_day02/gui_tkinter.py
+++ week05_day02/gui_tkinter.py
@@ -34,17 +34,56 @@ def simple02():
     main_wind = tk.Tk()
     main_wind.title("Second application")
 
-    c1 = tk.Canvas(bg='#ffff00')
+    c1 = tk.Canvas(main_wind, bg='#ffff00')
     c1.grid(row=0, column=0)
-    c2 = tk.Canvas(bg='#ff9911')
+
+    c2 = tk.Canvas(main_wind, bg='#ff9911')
     c2.grid(row=0, column=1)
-    c3 = tk.Canvas(bg='#00ffff')
+
+    c3 = tk.Canvas(main_wind, bg='#00ffff')
     c3.grid(row=1, column=0)
-    c4 = tk.Canvas(bg='#ff00ff')
+
+    c4 = tk.Canvas(main_wind, bg='#ff00ff')
     c4.grid(row=1, column=1)
+
+    for i in range(5):
+        tk.Label(c3, bg='#00ff00', text=f'0{i}').grid(row=0, column=i)
+        tk.Label(c3, bg='#00ff00', text=f'{i}0').grid(row=i, column=0)
 
     main_wind.mainloop()
 
 
+def simple03():
+    root = tk.Tk()
+
+    root.title('Radio buttons example')
+    root.geometry('300x300')
+
+    var = tk.IntVar()
+
+    button_label = tk.Label(root, text='Picked option is: ')
+    button_label.pack(side=tk.LEFT)
+
+    def print_option():
+        button_label.configure(text=f'Picked option is: {var.get()}')
+
+    tk.Label(root,
+             text='Choose option:',
+             justify=tk.LEFT,
+             ).pack()
+    tk.Radiobutton(root,
+                   text='Option1',
+                   variable=var,
+                   value=1,
+                   command=print_option).pack()
+    tk.Radiobutton(root,
+                   text='Option2',
+                   variable=var,
+                   value=2,
+                   command=print_option).pack()
+
+    root.mainloop()
+
+
 if __name__ == '__main__':
-    simple02()
+    simple03()
